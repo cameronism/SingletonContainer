a63f914bf415c30328e21e1d8c3ab51ffa7ae84a
Cameron Jordan <cjordan@cameronism.com>
2015-02-05 18:12:09 -0800

    initial commit

---
+++ SimpleContainer.Tests/BuilderTests.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SimpleContainer.Tests
+{
+	public class BuilderTests
+	{
+		interface IDep { }
+		class Dep1 : IDep {  }
+
+		[Fact]
+		public void Register()
+		{
+			var builder = new Builder();
+			var reg = builder.Register<Dep1>();
+			Assert.NotNull(reg);
+
+			builder.Build();
+
+			var container = builder.Container;
+			Assert.NotNull(container);
+
+			var instance = container.Resolve<Dep1>();
+			Assert.NotNull(instance);
+		}
+
+		[Fact]
+		public void Singletons()
+		{
+			var builder = new Builder();
+			builder.Register<Dep1>();
+			builder.Build();
+			var container = builder.Container;
+
+			var instance1 = container.Resolve<Dep1>();
+			var instance2 = container.Resolve<Dep1>();
+			Assert.Same(instance1, instance2);
+		}
+
+		[Fact]
+		public void Abstractions()
+		{
+			var builder = new Builder();
+			builder.Register<Dep1>().As<IDep>();
+			builder.Build();
+			var container = builder.Container;
+
+			var instance1 = container.Resolve<Dep1>();
+			var instance2 = container.Resolve<IDep>();
+			Assert.Same(instance1, instance2);
+		}
+
+		[Fact]
+		public void RejectBadAbstractions()
+		{
+			var builder = new Builder();
+
+			Assert.Throws<RegistrationFailedException>(() =>
+			{
+				builder.Register<Dep1>().As<IList<int>>();
+			});
+		}
+
+		[Fact]
+		public void RegistrationWithoutSelf()
+		{
+			var builder = new Builder();
+			builder.Register<Dep1>().As<IDep>().WithoutSelf();
+			builder.Build();
+			var container = builder.Container;
+
+			Assert.NotNull(container.Resolve<IDep>());
+			Assert.Throws<ResolutionFailedException>(() =>
+			{
+				container.Resolve<Dep1>();
+			});
+		}
+	}
+}
---
+++ SimpleContainer/Builder.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleContainer
+{
+	public interface IContainer
+	{
+		T Resolve<T>();
+
+		/// <summary>
+		/// Returns all registrants castable to T
+		/// </summary>
+		IList<T> OfType<T>();
+	}
+
+	public class DependencyCycleException : Exception
+	{
+
+	}
+
+	public class ContainerNotBuiltException : Exception
+	{
+	}
+
+	public class ContainerAlreadyBuiltException : Exception
+	{
+	}
+
+	public class ResolutionFailedException : Exception
+	{
+	}
+
+	public class RegistrationFailedException : Exception
+	{
+
+	}
+
+	public class Builder
+	{
+		#region inner types
+		public interface IRegistration
+		{
+			IRegistration As<T>();
+
+			// TODO look at what autofac calls this
+			/// <summary>
+			/// Makes current registration only available as type(s) registered with As() calls
+			/// </summary>
+			/// <returns></returns>
+			IRegistration WithoutSelf();
+		}
+
+		class Registration : IRegistration
+		{
+
+			public object Instance;
+			readonly Builder _Builder;
+			readonly Type _Type;
+
+			public Registration(Builder builder, Type type)
+			{
+				this._Builder = builder;
+				this._Type = type;
+			}
+
+			public IRegistration As<T>()
+			{
+				if (!typeof(T).IsAssignableFrom(_Type))
+				{
+					throw new RegistrationFailedException();
+				}
+
+				lock (_Builder._Gate)
+				{
+					_Builder.VerifyNotBuilt();
+					_Builder._Registrations[typeof(T)] = this;
+				}
+
+				return this;
+			}
+
+			public IRegistration WithoutSelf()
+			{
+				lock (_Builder._Gate)
+				{
+					_Builder.VerifyNotBuilt();
+					_Builder._Registrations.Remove(_Type);
+				}
+				return this;
+			}
+
+			public void Create()
+			{
+				if (Instance == null) Instance = Activator.CreateInstance(_Type);
+			}
+		}
+
+		class ContainerImp : IContainer
+		{
+			readonly Builder _Builder;
+
+			public ContainerImp(Builder builder)
+			{
+				_Builder = builder;
+			}
+			public T Resolve<T>()
+			{
+				_Builder.VerifyAlreadyBuilt();
+
+				Registration reg;
+				lock (_Builder._Gate)
+				{
+					_Builder._Registrations.TryGetValue(typeof(T), out reg);
+				}
+
+				if (reg == null) throw new ResolutionFailedException();
+
+				return (T)reg.Instance;
+			}
+
+
+			public IList<T> OfType<T>()
+			{
+				_Builder.VerifyAlreadyBuilt();
+
+				var items = new Dictionary<Registration, T>(_Builder._Registrations.Count);
+
+			}
+		}
+		#endregion
+
+		#region members
+		object _Gate = new object();
+		Dictionary<Type, Registration> _Registrations = new Dictionary<Type, Registration>();
+		ContainerImp _Container;
+		#endregion
+
+		void VerifyNotBuilt()
+		{
+			if (_Container != null) throw new ContainerAlreadyBuiltException();
+		}
+
+		void VerifyAlreadyBuilt()
+		{
+			if (_Container == null) throw new ContainerNotBuiltException();
+		}
+
+		public IRegistration Register<T>()
+		{
+			var reg = new Registration(this, typeof(T));
+			lock (_Gate)
+			{
+				VerifyNotBuilt();
+				_Registrations[typeof(T)] = reg;
+			}
+			return reg;
+		}
+		
+		public IContainer Container
+		{
+			get 
+			{
+				VerifyAlreadyBuilt();
+				return _Container;
+			}
+		}
+		
+		public void Dispose()
+		{
+		}
+
+		public void Build()
+		{
+			lock (_Gate)
+			{
+				if (_Container != null)
+				{
+					throw new ContainerAlreadyBuiltException();
+				}
+
+				_Container = new ContainerImp(this);
+
+				foreach (var reg in _Registrations.Values)
+				{
+					reg.Create();
+				}
+			}
+		}
+	}
+}
