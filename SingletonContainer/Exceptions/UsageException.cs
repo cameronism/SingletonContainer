38aff375d5ff4a6e15c78dba5fe71e45e3f0ec94
Cameron Jordan <cjordan@cameronism.com>
2015-02-09 10:51:08 -0800

    Rename BuilderException to UsageException

---
+++ SingletonContainer/Exceptions/UsageException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonContainer.Exceptions
+{
+	public abstract class UsageException : Exception
+	{
+	}
+
+	public class ContainerNotBuiltException : UsageException
+	{
+	}
+
+	public class ContainerAlreadyBuiltException : UsageException
+	{
+	}
+
+	public class RegistrationFailedException : UsageException
+	{
+	}
+}
